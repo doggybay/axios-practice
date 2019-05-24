1ebb637f1111ede30130bc3835e435bbc2359a7d
doggybay <julio.miranda12@gmail.com>
2019-05-23 23:10:41 -0700

    first commit

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ axios.get('https://jsonplaceholder.typicode.com/posts').then(postsResponse => {
   let posts = postsResponse.data;
   axios.get('https://jsonplaceholder.typicode.com/comments').then(commentsResponse => {
     let comments = commentsResponse.data;
-    
-    console.log('posts:', posts);
-    console.log('comments', comments);
+    //console.log('posts:', posts);
+    //console.log('comments', comments);
     posts.forEach(post => {
       posts.comments = comments.filter(comment => comment.postId === post.id);
-
-      //code goes in here....e
+      //console.log(posts.comments);
+      //code goes in here....
+      console.log('posts:', posts);
     });
     
   });
